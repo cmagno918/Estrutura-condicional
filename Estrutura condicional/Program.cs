4e7c71e8a94373694972960985a8841c0749f759
Carlos Magno <cmagno918@gmail.com>
2021-09-16 18:30:36 -0300

    Add files via upload

--- Estrutura condicional/Program.cs
+++ Estrutura condicional/Program.cs
@@ -1,30 +1,30 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Estrutura_condicional
+namespace Raiz_Quadrada_URI_1036__
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int horas;
+            double a, b, c, delta, r1, r2;
 
-            Console.WriteLine("Que horas são? ");
-            horas = int.Parse(Console.ReadLine());
+            string[] vet = Console.ReadLine().Split(' ');
 
-             if (horas < 12)
-            {
-                Console.WriteLine("Bom dia");
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0 || delta < 0.0) {
+                Console.WriteLine("Impossivel calcular");
             }
-            else
-            {
-                if (horas < 18)
-                {
-                    Console.WriteLine("Boa tarde");
-                }
-                else
-                {
-                    Console.WriteLine("Boa noite");
-                }
+            else {
+                r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
             }
         }
     }
